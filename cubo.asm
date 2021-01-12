5626de0879b6339e40ebe37f350930b244fa94ad
Jean <74760756+CoPr0c@users.noreply.github.com>
2021-01-12 00:23:27 -0300

    Update cubo.asm

--- cubo.asm
+++ cubo.asm
@@ -13,7 +13,7 @@ mul $t2,$t1,$t1		# $t2 = $t1 * $t1
 mul $t3,$t1,$t2 	# $t3 = $t1 * $t2
 li $v0, 4		# Preparando o processador pra receber uma string.
 la $a0, cubo		# Ler endereço de memória da variável cubo e jogar dentro do registrador $a0.
-syscall			# Executar as instruções.
+syscall			# Executar as instrucoes.
 li $v0, 1		# li (load immediate) ler imediatamente o registrador $v0 que vai receber um número inteiro.
 la $a0, ($t3)		# la (load address) $a0 = ($t3).
-syscall			# Executar instruções.
+syscall			# Executar instrucoes.
